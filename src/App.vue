c2f56b64cfa53f98ee6b0c1e1506e379fb214775
shang <yopshang@gmail.com>
2021-03-14 08:34:01 +0800

    add: setup

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <!-- <img src="./assets/logo.png"> -->
+    <index/>
+  </div>
+</template>
+
+<script>
+import index from './components/index'
+
+export default {
+  name: 'App',
+  components: {
+    index,
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
